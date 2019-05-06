6ca51299eda4fdeca9893440f5beaac86bf2f13e
merestrickland <mere.strickland@gmail.com>
2019-05-06 11:47:47 -0400

    added google map

--- src/components/Itinerary.js
+++ src/components/Itinerary.js
@@ -1,18 +1,25 @@
 import React from 'react';
 
+import MapContainer from './MapContainer'
+
 
 class Itinerary extends React.Component {
 
     //Itinerary takes in dates and location from App
-    
+
    state = {
        toDoList: []
    }
 
     render() {
+
+        
+
         return(
             <div>
                 <h1>Trip Itinerary</h1>
+                <MapContainer destinationName={this.props.destinationName} />
+                
                 <ul>
 
                 </ul>
@@ -24,3 +31,4 @@ class Itinerary extends React.Component {
 export default Itinerary;
 
 //layout of itinerary includes list of things to do -> almost as a form where the input will be the key-value pairs
+
