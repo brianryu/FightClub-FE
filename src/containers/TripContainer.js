67b26344ca70968f942229cd14106532f5c37359
merestrickland <mere.strickland@gmail.com>
2019-05-07 12:31:51 -0400

    routing

--- src/containers/TripContainer.js
+++ src/containers/TripContainer.js
@@ -1,69 +1,83 @@
-import React from 'react';
+import React from "react";
+import { Route, Switch, Link } from "react-router-dom";
 
+import TripList from "./TripList";
+import PlanForm from "../components/PlanForm";
 
-import TripList from './TripList';
-import PlanForm from '../components/PlanForm';
+class TripContainer extends React.Component {
+  state = {
+    trips: []
+  };
 
-
-
-
-
-class TripContainer extends React.Component{
-
-    
-
-    state = {
-        trips: []
-       
-      }
-      
-      componentDidMount(){
-        fetch("http://localhost:3005/api/v1/trips")
-        .then(response => response.json())
-        .then(tripArray => {
-          this.setState({
-              trips: tripArray
-          })
-        })
-      }
-
-  
-
-    newTrip = (event, tripObj) => {
-        event.preventDefault();
-        console.log(event)
-        let newArray = [...this.props.trips, tripObj]
+  componentDidMount() {
+    fetch("http://localhost:3005/api/v1/todos")
+      .then(response => response.json())
+      .then(tripArray => {
+ 
         this.setState({
-            trips: newArray
-        })
-        console.log(this.props.trips)
-    }
+          trips: tripArray
+        });
+      });
+  }
 
+  newTrip = (event, tripObj) => {
+    event.preventDefault();
+    console.log(event);
+    let newArray = [...this.props.trips, tripObj];
+    this.setState({
+      trips: newArray
+    });
+    console.log(this.props.trips);
+  };
 
+  handleSubmit = (event, taskObj) => {
+    event.preventDefault();
+    console.log(taskObj);
+  };
 
-    handleSubmit = (event, taskObj) => {
-        event.preventDefault();
-        console.log(taskObj)
-    }
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route path="/trips/:id" render={props => {
+              console.log(props)
+              
+              
+              const foundTrip = this.state.trips.find(trip => {
+                    console.log(trip.id)
+                  return trip.id === parseInt(props.match.params.id)
+              })
+            //   debugger;
+              if (foundTrip === undefined) {
+                  props.history.push("/")
+                  return
+              } else {
+                  return (
+                      <TripList eachTrip={foundTrip}/>
+                  )
+              }
+          }}/>
 
-    render() {
-        return (
-            <div>
-               
-                <PlanForm newTrip={this.newTrip}/>
-    
-                <h1>Your Trips</h1>
-                {this.state.trips.map(trip => {
-                    return <TripList eachTrip={trip}  />  
-                })} 
-               
-                
-            </div>      
-        )
-    }
+          <Route
+            path="/trips"
+            render={props => {
+              return (
+                <div>
+                  <PlanForm newTrip={this.newTrip} />
 
-
-
+                  <h1>Your Trips</h1>
+                  {this.state.trips.map(tripObj => {
+                    console.log(tripObj.trip.name);
+                    return <TripList eachTrip={tripObj.trip} />;
+                  })}
+                </div>
+              );
+            }}
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default TripContainer
+export default TripContainer;
